93fd6a3a84a7a498e79a0f1fd833af0a3eed7b3a
carlow <carlesonkalebe2016@gmail.com>
2024-08-23 23:50:14 -0400

    init

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var inputValue: Double? = 0.0
+    private var inputValue2: Double? = null
+    private var currentOperator: Operator? = null
+    private var result: Double? = null
+    private var equation: StringBuilder = StringBuilder().append(ZERO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    enum class Operator {
+        ADDITION,
+        SUBTRACTION,
+        MULTIPLICATION,
+        DIVISION
+    }
+
+    private companion object{
+        const val DECIMAL_POINT = "."
+        const val ZERO = "0"
+        const val DOUBLE_ZERO = "00"
+        const val MINUS = "-"
+
+    }
+
 }
